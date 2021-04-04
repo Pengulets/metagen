e6844c9e7be9bad0115f57ce92b433745bc606f5
QuantumlyTangled <ethoarx@gmail.com>
2021-04-05 00:12:58 +0200

    feat: loot table utility

---
+++ src/lib/LootItem.ts
@@ -0,0 +1,10 @@
+export type LootItemValue = string | [name: string, value: string];
+
+export class LootItem {
+	public constructor(public item: LootItemValue, public dropRate: number) {
+		this.item = item;
+		this.dropRate = dropRate;
+	}
+}
+
+export default LootItem;
---
+++ src/lib/LootTable.ts
@@ -0,0 +1,27 @@
+import LootItem, { LootItemValue } from './LootItem';
+import { randomInRange } from './utils/utils';
+
+export class LootTable {
+	public items: LootItem[] = [];
+
+	public addItem(item: string | [name: string, value: string], dropRate: number) {
+		this.items.push(new LootItem(item, dropRate));
+		return this;
+	}
+
+	public dropItem(): LootItemValue {
+		// Extract a random integer between 0 and "totalWeight"
+		let randomNumber: number = randomInRange(0, this.computeTotalWeight());
+
+		// Pick the item to drop
+		for (const item of this.items)
+			if (randomNumber <= item.dropRate) return item.item;
+			else randomNumber -= item.dropRate;
+
+		return this.items[Math.floor(Math.random() * this.items.length)].item;
+	}
+
+	protected computeTotalWeight(): number {
+		return this.items.map((item: LootItem) => item.dropRate).reduce((total: number, sum: number) => (total += sum));
+	}
+}
