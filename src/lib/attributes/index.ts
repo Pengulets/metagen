162dba7b49c4baa7c7d05283503e8908682a33a9
QuantumlyTangled <ethoarx@gmail.com>
2021-04-05 07:33:43 +0200

    feat: aggregate all attribute calls

---
+++ src/lib/attributes/index.ts
@@ -0,0 +1,33 @@
+import { base } from './base';
+
+export const attributes = [base];
+
+export const callAttributes = () => {
+	const outMeta: Attribute[] = [];
+	const outIgloo: IglooStep[] = [];
+
+	attributes.forEach((attr) => {
+		const attribute = attr.call(null);
+
+		outMeta.push(attribute[0]);
+		outIgloo.push(attribute[1]);
+	});
+
+	return {
+		metadata: outMeta,
+		igloo: outIgloo
+	};
+};
+
+export interface Attribute {
+	trait_type: string;
+	value: number | string;
+	display_type?: string;
+}
+
+export interface IglooStep {
+	Step: number;
+	Value: string | number;
+}
+
+export type AttributeReturn = [attribute: Attribute, Igloo: IglooStep];
