2ed7af580a5e4eb313c03d02141a8a775bfd069c
QuantumlyTangled <ethoarx@gmail.com>
2021-04-05 07:33:24 +0200

    feat: some basic background colours

---
+++ src/lib/attributes/base.ts
@@ -0,0 +1,30 @@
+import { AttributeReturn } from '.';
+import { LootTable } from '../LootTable';
+
+// https://www.schemecolor.com/pastella.php
+export const Base = new LootTable() //
+	.addItem(['Dutch White', '#F2DDC0'], 100)
+	.addItem(['Pale Robin Egg Blue', '#90DAD9'], 75)
+	.addItem(['Spanish Pink', '#F3BEBC'], 75)
+	.addItem(['Almond', '#EBDED1'], 50)
+	.addItem(['Cherry Blossom Pink', '#F8B9C5'], 50)
+	.addItem(['Light Steel Blue', '#AFC0E3'], 40)
+	.addItem(['Light Orchid', '#E7A8E3'], 20)
+	.addItem(['Maximum Blue Purple', '#A09CF3'], 20)
+	.addItem(['Pink Lavender', '#DAB5D2'], 20)
+	.addItem(['Magic Mint', '#ADEAC3'], 10);
+
+export const base = (): AttributeReturn => {
+	const _base = Base.dropItem() as [name: string, value: string];
+
+	return [
+		{
+			trait_type: 'Background',
+			value: _base[0]
+		},
+		{
+			Step: 0,
+			Value: _base[1]
+		}
+	];
+};
